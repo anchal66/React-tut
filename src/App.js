c2c24a68e08eff24a3c657961e147deef621159d
Avinash Anchal <anchalavinash66@gmail.com>
2020-04-29 09:51:25 +0530

    using State

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // State can be used on class which extends compont we can use other property but state is specl property
+  //If properties of state changes it reders the changed value in HTML too
+  state ={
+    persons:[
+      {name: 'Avinash', age: 27},
+      {name: 'Avi', age: 23}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hiii</h1>
         <p>Welcome</p>
-        <Person name="Avinash" age="27">Hiii there!</Person>
-        <Person name="Avi" age="23"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hiii there!</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const person = (props)=> {
     return <div>
-        <p>I m a {props.name} with number {Math.floor(Math.random()*100)} and age {props.age}</p>
+        <p>I m a {props.name} with age {props.age}</p>
         <p>{props.children}</p>
         </div>
 };
