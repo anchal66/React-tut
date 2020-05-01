89fac381d1ff88708117c877393504c555a4e3f7
Avinash Anchal <anchalavinash66@gmail.com>
2020-05-01 09:08:04 +0530

    SyldComponet Intro

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import './Person.css'
+import styled from 'styled-components;'
+
+const StyledDiv = styled.div`
+width: 60%;
+margin: auto;
+border: 1px solid #eee;
+box-shadow: 0 2px 3px #ccc;
+padding: 16px;
+text-align: center;
+@media(min-width: 500px){
+    width: 450px;
+}`
 
 const person = (props) => {
     const style = {
@@ -7,11 +19,14 @@ const person = (props) => {
             width: '450px'
         }
     }
-    return <div className="Person" style={style}>
-        <p onClick={props.click}>I m a {props.name} with age {props.age}</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
+    return (
+        // <div className="Person" style={style}>
+        <StyledDiv>
+            < p onClick={props.click} > I m a {props.name} with age {props.age}</p >
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </StyledDiv>
+    )
 };
 
 export default person;
