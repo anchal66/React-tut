5a76501261f23e25949284595e62533a9a9aa1a1
Avinash Anchal <anchalavinash66@gmail.com>
2020-05-04 17:33:44 +0530

    PureComponent to auto addCheck ifPropsCHngExcecute

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,21 +1,21 @@
-import React, {Component} from 'react'
+import React, {PureComponent} from 'react'
 import Person from './Person/Person'
 
-class Persons extends Component {
+class Persons extends PureComponent {
   static getDerivedStateFromProps(props, state){
     console.log('Perons getDerivedStateFromProps',props);
     return state;
   }
 
-  shouldComponentUpdate(nextProps, nextState){
-    console.log('Perons shouldComponentUpdate')
-    if(nextProps.persons !== this.props.persons){
-      return true
-    }
-    return false;
-    //note persons caontains onlu pointer not object but ir will work ccoz in code we are craeting new o
-    //object of persons [... per] so new pointer is genetertaed
-  }
+  // shouldComponentUpdate(nextProps, nextState){
+  //   console.log('Perons shouldComponentUpdate')
+  //   if(nextProps.persons !== this.props.persons){
+  //     return true
+  //   }
+  //   return false;
+  //   //note persons caontains onlu pointer not object but ir will work ccoz in code we are craeting new o
+  //   //object of persons [... per] so new pointer is genetertaed
+  // }
    
   getSnapshotBeforeUpdate(prevProps, nextProps){
     console.log('Perons getSnapshotBeforeUpdate cn be used in scrolling');
